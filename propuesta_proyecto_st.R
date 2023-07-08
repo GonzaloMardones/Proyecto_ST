bf1544e644ccd4a7d8eaeab38e332c5bc061f57b
Gonzalo Mardones <gonzalo-a@hotmail.com>
2023-07-08 10:57:56 -0400

    first version

--- propuesta_proyecto_st.R
+++ propuesta_proyecto_st.R
@@ -1,25 +1,78 @@
-# Librerias
 library(dplyr)
 library(rio)
 library(ggplot2)
 library(descomponer)
-library(dplyr)
-
+library(lubridate)
 library(forecast)
 library(tseries)
 
-df <- rio::import("/Users/macbookpro/Desktop/MAGISTER/Series_Tiempo/Proyecto/base_tasa_participacion.xlsx", sheet="Datos")
-df$Fecha <- as.Date(paste0("01.", df$Periodo), format = "%d.%b.%Y")
+ruta <- "/Users/macbookpro/Desktop/MAGISTER/Series_Tiempo/Proyecto/"
+tasa_participacion <- "base_tasa_participacion.xlsx"
+ocupacion <- "ocupados_por_grupo_de_ocupacion.xlsx"
+imacec <- "imacec.xlsx"
+
+# Carga de datos (tasa participacion, ocupacion, imacec)
+df_participacion <- rio::import(file.path(ruta, tasa_participacion), sheet="Datos")
+df_ocupacion <- rio::import(file.path(ruta, ocupacion))
+df_imacec <- rio::import(file.path(ruta, imacec))
+
+#Parse Fecha Participacion - Ocupacion - Imacec
+df_participacion$Fecha <- as.Date(paste0("01.", df_participacion$Periodo), format = "%d.%b.%Y")
+df_participacion_filtrados <- subset(df_participacion, Fecha <= as.Date("2022-12-31"))
+df_ocupacion_filtrados <- subset(df_ocupacion, Periodo <= as.Date("2022-12-31"))
+df_imacec_filtrados <- subset(df_imacec, Fecha <= as.Date("2022-12-31"))
 
 
 # Serie de tiempo General
-serie_tiempo_general <- ts(data = df[, -1], start = c(2010, 3), frequency = 12)
-serie_tiempo <- ts(data = df$Tasa, start = c(2010, 3), frequency = 12)
+serie_tiempo_general <- ts(data = df_participacion_filtrados[, -1], start = c(2010, 3), frequency = 12)
 
-# Asignar las fechas a ST Tasa
-time(serie_tiempo) <- datos$Fecha
+# Serie de tiempo participacion
+serie_tiempo <- ts(data = df_participacion_filtrados$Tasa, start = c(2010, 3), frequency = 12)
+
+# --------- DUMMIES ----------
+# DUMMY - ABRIL 2020
+df_participacion_filtrados$dummy_ABR_2020 <- ifelse(df_participacion_filtrados$Fecha=='2020-04-01', 1, 0)
+# Crear las columnas "mes" y "año"
+df_participacion_filtrados <- df_participacion_filtrados %>%
+  mutate(mes = match(month(Fecha, label = TRUE), month.abb),año = year(Fecha))
+
+# Crear variables dummy para los meses
+meses_dummy <- model.matrix(~ factor(df_participacion_filtrados$mes) - 1)
+
+# Crear un dataframe con las variables exógenas
+exogenas <- data.frame(meses_dummy)
+
+# Agregar variables macroeconómicas, ocupacion, imacec, al dataframe exogenas para crear modelo
+modelo_datos <- cbind(serie_tiempo,
+                      imacec = df_imacec_filtrados$`Imacec desestacionalizado (promedio móvil de 3 meses)`, 
+                      ocupacion= df_ocupacion_filtrados$Total, 
+                      exogenas, 
+                      df_participacion_filtrados$dummy_ABR_2020)
+
+# Ajustar un modelo de regresión lineal
+modelo_regresion <- lm(serie_tiempo ~ ., data = modelo_datos)
+summary(modelo_regresion)
+
+
+# Crear un dataframe para realizar predicciones
+nuevas_exogenas <- data.frame(meses_dummy, 
+                              imacec =  df_imacec_filtrados$`Imacec desestacionalizado (promedio móvil de 3 meses)`, 
+                              ocupacion= df_ocupacion_filtrados$Total) 
+
+
+nuevas_fechas <- seq(as.Date("2020-03-01"), as.Date("2022-12-01"), by = "month")
+nuevos_datos <- data.frame(fecha = nuevas_fechas)
+nuevos_datos$prediccion <- predict(modelo_regresion, newdata = nuevas_exogenas)
+
+
+# Asignar las fechas a ST
 print(serie_tiempo)
 plot(serie_tiempo, xlab = "Fecha", ylab = "Tasa", main = "Tasa de Participación")
+
+fit <- auto.arima(serie_tiempo)
+fit
+plot(fit)
+plot(forecast(fit, h=24))
 
 # Serie de tiempo General
 print(serie_tiempo_general)
