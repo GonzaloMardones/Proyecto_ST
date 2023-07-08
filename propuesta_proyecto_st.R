2b8bb084d24878d618be84ed142ea8ded738dcdd
Gonzalo Mardones <gonzalo-a@hotmail.com>
2023-07-08 12:47:05 -0400

    first version

--- propuesta_proyecto_st.R
+++ propuesta_proyecto_st.R
@@ -31,7 +31,9 @@ serie_tiempo <- ts(data = df_participacion_filtrados$Tasa, start = c(2010, 3), f
 
 # --------- DUMMIES ----------
 # DUMMY - ABRIL 2020
-df_participacion_filtrados$dummy_ABR_2020 <- ifelse(df_participacion_filtrados$Fecha=='2020-04-01', 1, 0)
+#df_participacion_filtrados$dummy_ABR_2020 <- ifelse(df_participacion_filtrados$Fecha=='2020-04-01', 1, 0)
+
+
 # Crear las columnas "mes" y "año"
 df_participacion_filtrados <- df_participacion_filtrados %>%
   mutate(mes = match(month(Fecha, label = TRUE), month.abb),año = year(Fecha))
@@ -42,27 +44,44 @@ meses_dummy <- model.matrix(~ factor(df_participacion_filtrados$mes) - 1)
 # Crear un dataframe con las variables exógenas
 exogenas <- data.frame(meses_dummy)
 
+exogenas <- exogenas %>%
+  rename_with(~gsub("^factor\\.df_participacion_filtrados\\.mes\\.", "mes.", .), starts_with("factor.df_participacion_filtrados.mes."))
+
+
 # Agregar variables macroeconómicas, ocupacion, imacec, al dataframe exogenas para crear modelo
 modelo_datos <- cbind(serie_tiempo,
                       imacec = df_imacec_filtrados$`Imacec desestacionalizado (promedio móvil de 3 meses)`, 
                       ocupacion= df_ocupacion_filtrados$Total, 
-                      exogenas, 
-                      df_participacion_filtrados$dummy_ABR_2020)
+                      exogenas
+                      )
+#                      df_participacion_filtrados$dummy_ABR_2020)
 
 # Ajustar un modelo de regresión lineal
 modelo_regresion <- lm(serie_tiempo ~ ., data = modelo_datos)
 summary(modelo_regresion)
 
-
+meses_dummy_imacec <- model.matrix(~ factor(df_imacec$Mes) - 1)
 # Crear un dataframe para realizar predicciones
-nuevas_exogenas <- data.frame(meses_dummy, 
-                              imacec =  df_imacec_filtrados$`Imacec desestacionalizado (promedio móvil de 3 meses)`, 
-                              ocupacion= df_ocupacion_filtrados$Total) 
+nuevas_exogenas <- data.frame(meses_dummy_imacec, 
+                              imacec =  df_imacec$`Imacec desestacionalizado (promedio móvil de 3 meses)`, 
+                              ocupacion= df_ocupacion$Total
+                              ) 
 
+nuevas_exogenas <- nuevas_exogenas %>%
+  rename_with(~sub("^factor\\.df_imacec\\.Mes\\.", "exogenas.mes.", .))
 
-nuevas_fechas <- seq(as.Date("2020-03-01"), as.Date("2022-12-01"), by = "month")
+#nuevas_exogenas$dummy_ABR_2020 <- ifelse(df_participacion_filtrados$Fecha=='2020-04-01', 1, 0)
+
+nuevas_fechas <- seq(as.Date("2010-03-01"), as.Date("2023-05-01"), by = "month")
 nuevos_datos <- data.frame(fecha = nuevas_fechas)
-nuevos_datos$prediccion <- predict(modelo_regresion, newdata = nuevas_exogenas)
+
+nuevos_datos$prediccion  <- predict(modelo_regresion, newdata = nuevas_exogenas)
+
+ggplot() +
+  geom_line(data = df_participacion_filtrados, aes(x = Fecha, y = Tasa), color = "blue", linetype = "solid", size = 1) +
+  geom_line(data = nuevos_datos, aes(x = fecha, y = prediccion), color = "red", linetype = "dashed", size = 0.5) +
+  labs(x = "Fecha", y = "Venta de Créditos", title = "Datos Originales y Predicciones del Modelo") +
+  theme_minimal()
 
 
 # Asignar las fechas a ST
